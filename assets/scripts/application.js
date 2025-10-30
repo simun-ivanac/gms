734a2ec170fe45d39a02ba7884b3cbd12b0c7d4a
simun-ivanac <ivanac.simun@gmail.com>
2025-10-30 19:18:14 +0100

    register js scripts through dynamic import

--- assets/scripts/application.js
+++ assets/scripts/application.js
@@ -1,3 +1,11 @@
 /**
  * This is the main entry point for project scripts.
  */
+
+function dynamicImport(paths) {
+	paths.keys().forEach(paths);
+}
+
+// Register all scripts.
+// eslint-disable-next-line
+dynamicImport(require.context('./', true, /\.js$/));
