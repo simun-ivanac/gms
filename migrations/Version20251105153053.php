472d36775869e75092baf233beb6a23f7ed6884d
simun-ivanac <ivanac.simun@gmail.com>
2025-11-05 16:34:51 +0100

    set all migrations in one file

--- migrations/Version20251105153053.php
+++ migrations/Version20251105153053.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Migration Version20251025153107.
+ * Migration Version20251105153053.
  */
 
 declare(strict_types=1);
@@ -12,9 +12,9 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * Class Version20251025153107.
+ * Class Version20251105153053.
  */
-final class Version20251025153107 extends AbstractMigration
+final class Version20251105153053 extends AbstractMigration
 {
 	/**
 	 * Get the description of the migration.
@@ -38,7 +38,7 @@ final class Version20251025153107 extends AbstractMigration
 	{
 		// phpcs:disable
 		$this->addSql('CREATE TABLE `group` (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, price INT NOT NULL, currency VARCHAR(255) NOT NULL, repeatability VARCHAR(255) NOT NULL, num_of_sessions_per_week INT DEFAULT NULL, payment_frequency_in_days INT DEFAULT NULL, is_active TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-		$this->addSql('CREATE TABLE `member` (id INT AUTO_INCREMENT NOT NULL, photo VARCHAR(255) DEFAULT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, date_of_birth DATE NOT NULL, gender VARCHAR(255) DEFAULT NULL, email VARCHAR(255) DEFAULT NULL, phone_number VARCHAR(255) DEFAULT NULL, pin BIGINT NOT NULL, is_active TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+		$this->addSql('CREATE TABLE `member` (id INT AUTO_INCREMENT NOT NULL, photo_filename VARCHAR(255) DEFAULT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, date_of_birth DATE NOT NULL, gender VARCHAR(255) DEFAULT NULL, email VARCHAR(255) DEFAULT NULL, phone_number VARCHAR(255) DEFAULT NULL, pin BIGINT NOT NULL, is_active TINYINT(1) NOT NULL, is_deactivated TINYINT(1) DEFAULT NULL, is_deleted TINYINT(1) DEFAULT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', deactivated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', deleted_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
 		$this->addSql('CREATE TABLE plan (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, price INT NOT NULL, currency VARCHAR(255) NOT NULL, duration_in_days INT NOT NULL, are_visitations_limited TINYINT(1) NOT NULL, num_of_visitations_per_week INT DEFAULT NULL, is_active TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
 		$this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, team_member_role VARCHAR(255) NOT NULL, permissions JSON DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
 		$this->addSql('CREATE TABLE team_member (id INT AUTO_INCREMENT NOT NULL, photo VARCHAR(255) DEFAULT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, date_of_birth DATE NOT NULL, gender VARCHAR(255) DEFAULT NULL, email VARCHAR(180) NOT NULL, phone_number VARCHAR(255) NOT NULL, pin BIGINT NOT NULL, is_active TINYINT(1) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -56,11 +56,13 @@ final class Version20251025153107 extends AbstractMigration
 	// phpcs:ignore
 	public function down(Schema $schema): void
 	{
+		// phpcs:disable
 		$this->addSql('DROP TABLE `group`');
 		$this->addSql('DROP TABLE `member`');
 		$this->addSql('DROP TABLE plan');
 		$this->addSql('DROP TABLE role');
 		$this->addSql('DROP TABLE team_member');
 		$this->addSql('DROP TABLE messenger_messages');
+		// phpcs:enable
 	}
 }
