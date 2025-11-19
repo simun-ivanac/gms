2d9bbde3403a1da376ad1d3e01d7eb72ba17ad7a
simun-ivanac <ivanac.simun@gmail.com>
2025-11-19 19:12:21 +0100

    update reset password template

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -46,20 +46,20 @@ class ChangePasswordFormType extends AbstractType
 				'first_options' => [
 					'constraints' => [
 						new NotBlank([
-							'message' => 'Please enter a password',
+							'message' => 'Please enter a password.',
 						]),
 						new Length([
 							'min' => 12,
-							'minMessage' => 'Your password should be at least {{ limit }} characters',
+							'minMessage' => 'Your password should be at least {{ limit }} characters.',
 							'max' => 4096,
 						]),
 						new PasswordStrength(),
 						new NotCompromisedPassword(),
 					],
-					'label' => 'New password',
+					'label' => 'New Password *',
 				],
 				'second_options' => [
-					'label' => 'Repeat Password',
+					'label' => 'Repeat Password *',
 				],
 				'invalid_message' => 'The password fields must match.',
 				'mapped' => false,
