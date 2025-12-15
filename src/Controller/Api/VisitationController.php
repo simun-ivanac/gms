135d9b533bd6a6253a3d4c5541a55a5462bb9df8
simun-ivanac <ivanac.simun@gmail.com>
2025-12-15 20:07:21 +0100

    add logic for adding new visitation log

--- src/Controller/Api/VisitationController.php
+++ src/Controller/Api/VisitationController.php
@@ -10,9 +10,14 @@ namespace App\Controller\Api;
 
 use App\Entity\Member;
 use App\Entity\TeamMember;
+use App\Entity\Visitation;
+use App\Repository\MemberRepository;
+use App\Repository\TeamMemberRepository;
 use App\Repository\VisitationRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -21,6 +26,11 @@ use Symfony\Component\Serializer\SerializerInterface;
  */
 final class VisitationController extends AbstractController
 {
+	/**
+	 * API token.
+	 */
+	private const TEMP_VISITATION_TOKEN = 'Nud5jk7TFi!grHu';
+
 	/**
 	 * API main response.
 	 *
@@ -33,6 +43,93 @@ final class VisitationController extends AbstractController
 	public function index(VisitationRepository $repository, SerializerInterface $serializer): JsonResponse
 	{
 		$results['visitations'] = $repository->findAllEntries();
+
+		$jsonContent = $serializer->serialize($results, 'json');
+
+		return JsonResponse::fromJsonString($jsonContent);
+	}
+
+	/**
+	 * Create new visitation.
+	 *
+	 * @param Request                $request              Request.
+	 * @param EntityManagerInterface $entityManager        Entity Manager.
+	 * @param MemberRepository       $memberRepository     Member repository.
+	 * @param TeamMemberRepository   $teamMemberRepository Team member repository.
+	 * @param SerializerInterface    $serializer           Serializer.
+	 *
+	 * @return JsonResponse
+	 */
+	#[Route(path: '/api/visitations/new', name: 'visitation_api_new', methods: ['POST'])]
+	public function new(
+		Request $request,
+		EntityManagerInterface $entityManager,
+		MemberRepository $memberRepository,
+		TeamMemberRepository $teamMemberRepository,
+		SerializerInterface $serializer
+	): JsonResponse {
+		$token = $request->getPayload()->get('token');
+		$userType = $request->getPayload()->get('userType');
+		$userId = $request->getPayload()->get('userId');
+		$toCreate = true;
+
+		// Check token.
+		if ($token !== self::TEMP_VISITATION_TOKEN) {
+			$toCreate = false;
+			$statusCode = JsonResponse::HTTP_FORBIDDEN;
+			$results['message'] = 'Access denied. You are not allowed to proceed.';
+		}
+
+		// Check user type.
+		if ($userType !== 'member' && $userType !== 'teamMember') {
+			$toCreate = false;
+			$statusCode = JsonResponse::HTTP_BAD_REQUEST;
+			$results['message'] = 'User type field is invalid.';
+		}
+
+		// Check if user ID exists.
+		if (!$userId) {
+			$toCreate = false;
+			$statusCode = JsonResponse::HTTP_BAD_REQUEST;
+			$results['message'] = 'User ID field cannot be empty.';
+		}
+
+		// Check if user exists.
+		$findUser = $userType === 'member' ? $memberRepository->find($userId) : $teamMemberRepository->find($userId);
+
+		if (!$findUser) {
+			$toCreate = false;
+			$statusCode = JsonResponse::HTTP_BAD_REQUEST;
+			$results['message'] = 'User ID field is invalid.';
+		}
+
+		// Send error?
+		if (!$toCreate) {
+			$results['status'] = 'error';
+			$jsonContent = $serializer->serialize($results, 'json');
+			return JsonResponse::fromJsonString($jsonContent, $statusCode);
+		}
+
+		// Create entry.
+		$visitation = new Visitation();
+		$visitation->setStatus('success');
+		$visitation->setMessage('User entered the gym.');
+		$userType === 'member' ? $visitation->setMember($findUser) : $visitation->setTeamMember($findUser);
+		$entityManager->persist($visitation);
+		$entityManager->flush();
+
+		$results = [
+			'id' => $visitation->getId(),
+			'timestamp' => $visitation->getTimestamp(),
+			'status' => 'success',
+			'message' => 'User entered the gym.',
+		];
+
+		if ($userType === 'member') {
+			$results['member'] = (int) $userId;
+		} else {
+			$results['teamMember'] = (int) $userId;
+		}
 
 		$jsonContent = $serializer->serialize($results, 'json');
 
